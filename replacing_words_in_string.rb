3b7dc78d9502703ea8180ba012beafe92e95aa65
abitravers1989 <atravers4007@gmail.com>
2017-07-25 18:07:05 +0100

    replacing words in a string kuta

--- replacing_words_in_string.rb
+++ replacing_words_in_string.rb
@@ -10,3 +10,27 @@ def part(arr)
     "Mine's a Pint#{'!' * terms}"
  end
 end
+
+or
+
+
+PART_ARRAY = ["Partridge",
+"PearTree",
+"Chat",
+"Dan",
+"Toblerone",
+"Lynn",
+"AlphaPapa",
+"Nomad"]
+
+def part(arr)
+  partridge_count = 0
+  arr.each do |word|
+    partridge_count += 1 if PART_ARRAY.include?(word)
+  end
+  if partridge_count > 0
+    "Mine's a Pint" + '!'* partridge_count
+  else
+    "Lynn, I've pierced my foot on a spike!!"
+  end
+end
