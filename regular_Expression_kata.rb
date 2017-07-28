9ca90c7d6ac4bbfcaebc2f2cd1a75e8270b538f5
abitravers1989 <atravers4007@gmail.com>
2017-07-28 10:33:36 +0100

    7 Kyu kata on Regula Expression

---
+++ regular_Expression_kata.rb
@@ -0,0 +1,10 @@
+def getCount(inputStr)
+  inputStr.count("/[aeiou]/")
+end
+
+
+=begin
+
+Return the number (count) of vowels in the given string.
+
+We will consider a, e, i, o, and u as vowels for this Kata.
